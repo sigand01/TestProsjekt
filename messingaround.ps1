7674547773df124abd09641a6c5ce83dbc81bcde
Sig <sigand01@gmail.com>
2018-01-27 20:29:26 +0100

    Display running processes

--- messingaround.ps1
+++ messingaround.ps1
@@ -1,1 +1,8 @@
-Get-Service | Where-Object {$_.Status -eq "Running"}
+$prosesser = Get-Service | Where-Object {$_.Status -eq "Running"}
+Write-Host "Aktive prosesser:" -ForegroundColor Green
+Write-Host ""
+$prosesser
+Write-Host ""
+Write-Host "Antall prosesser: " $prosesser.count -ForegroundColor Green
+Pause
+
